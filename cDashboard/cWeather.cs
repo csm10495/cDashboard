e4f24c38ab889fb31ba2c4f104801f06a833e7cc
csm10495 <csm10495@gmail.com>
2014-11-06 15:26:00 -0500

    removed ssl from query, Yahoo has some bad certificates

--- cDashboard/cWeather.cs
+++ cDashboard/cWeather.cs
@@ -125,7 +125,7 @@ namespace cDashboard
         {
             //at this point a WOEID should exist
 
-            dict_y_forecast = getDictFromJsonUrl("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D" + WOEID + "&format=json&u=f");
+            dict_y_forecast = getDictFromJsonUrl("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D" + WOEID + "&format=json&u=f");
         }
 
         /// <summary>
