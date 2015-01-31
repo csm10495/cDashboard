22d3b06a248adc986cfa72479d9ab9711b7131a2
csm10495 <csm10495@gmail.com>
2015-01-31 02:01:43 -0500

    boilerplate updates / trying to make them the same across the board

--- cDashboard/cAbout.cs
+++ cDashboard/cAbout.cs
@@ -1,6 +1,7 @@
 ﻿//This file is part of cDashboard
+//cDashboard - An information-based overlay for Microsoft Windows
 //cAbout - An about box for cDashboard
-//(C) Charles Machalow 2014 under the MIT License
+//(C) Charles Machalow under the MIT License
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
--- cDashboard/cStopwatch.cs
+++ cDashboard/cStopwatch.cs
@@ -1,7 +1,7 @@
 ﻿//This file is part of cDashboard
 //cDashboard - An information-based overlay for Microsoft Windows
 //cStopwatch - A stopwatch for cDashboard
-//(C) Charles Machalow 2014 under the MIT License
+//(C) Charles Machalow under the MIT License
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
