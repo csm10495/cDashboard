1012b59479ed42894771181882c42c18b41428ce
csm10495 <csm10495@gmail.com>
2015-02-02 21:41:56 -0500

    update version info

--- cDashboard/Properties/AssemblyInfo.cs
+++ cDashboard/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("cDashboard")]
-[assembly: AssemblyDescription("An overlay for Windows")]
+[assembly: AssemblyDescription("An Infrormation-based overlay for Windows")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("csm10495")]
 [assembly: AssemblyProduct("cDashboard")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.3.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
